f8a3a5d99292836dbba9f257018148b48197f77a
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-09-29 20:12:08 +0200

    Ejercicio3

---
+++ lucia_casos_de_nombre_3.py
@@ -0,0 +1,5 @@
+# Casos de nombre: Almacena el nombre de una persona en una variable y luego imprime ese nombre en minúsculas, mayúsculas y con la primera letra de cada palabra en mayúscula (formato título).
+nombre="Lucia"
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.title())
