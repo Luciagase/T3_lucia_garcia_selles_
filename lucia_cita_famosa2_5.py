16edb48b0b5680bafe858664f9d3a13fe8755413
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-09-29 21:16:44 +0200

    ejercicio5

---
+++ lucia_cita_famosa2_5.py
@@ -0,0 +1,4 @@
+#5) Cita famosa 2: Repite el ejercicio 4, pero esta vez almacena el nombre de la persona famosa en una variable llamada "famous_person" (persona famosa). Luego, crea tu mensaje y guárdalo en una nueva variable llamada "message" (mensaje). Imprime tu mensaje.
+famous_person="Lord Acton"
+message="'El poder tiende a corromper, el poder absoluto corrompe absolutamente'."
+print(message,"-",famous_person)
