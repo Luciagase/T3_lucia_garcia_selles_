1ae58a4d50a930941c0490e9de0976d393992dd8
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-09-29 19:56:58 +0200

    tipos de datos

--- lucia_tipos_NUMERO.py
+++ lucia_tipos_NUMERO.py
@@ -1,5 +1,16 @@
-# Existen 4 tipos de datos en Python:
-# 1. Enteros (int)
-# 2. Flotantes (float)
-# 3. Strings (str)
-# 4. Booleanos (bool)
+# 1) Indica un ejemplo de un tipo de dato:
+    # 1. Entero (int)
+a=5
+print(type(a))
+
+    # 2. Flotante (float)
+b=4,2
+print(type(b))
+
+    # 3. String (str)
+c="mesa"
+print(type(c))
+
+    # 4. Booleano (bool)
+d=True
+print(type(d))
