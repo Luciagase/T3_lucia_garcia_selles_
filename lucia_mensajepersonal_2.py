6dad8949ccdac0ef9d2bf83bcd8076a327a7024d
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-09-29 20:09:49 +0200

    ejercicio 2

---
+++ lucia_mensajepersonal_2.py
@@ -0,0 +1,3 @@
+# 2)Mensaje personal: Almacena el nombre de una persona en una variable y muestra un mensaje a esa persona. Tu mensaje debería ser simple, como "Hola Eric, ¿te gustaría aprender algo de Python hoy?
+nombre="Lucia"
+print("Hola",nombre,"¿te gustaría aprender algo de Python hoy?")
